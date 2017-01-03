ddbc3479cef9cd32ec93ebaf5e8371441a25a1ca
jwq <jiangwenqiang@jwq.com>
2017-01-03 19:11:04 +0800

    'todo基本内容显示''

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
 (function (window) {
 	'use strict';
-
-	// Your starting point. Enjoy the ride!
-
+	//引用angular代码
+//创建模块
+//	创建控制器
+//	验证代码正确性
+	var app = angular.module('todoApp',[]);
+	//防止压缩失效
+	app.controller('todoCtrl', ['$scope',function ($scope) {
+		//test
+		//$scope.name='todomvc';
+		//查看、初始化数据
+		//todo显示的列表
+		$scope.todoList = [
+			{id:getID(),text:'css',status:true},
+			{id:getID(),text:'js',status:false},
+			{id:getID(),text:'jquery',status:false},
+		];
+		//获取随机数用于给信息创建id
+		function getID(){
+			return Math.random();
+		}
+	}])
 })(window);
